64e5c7e9b6d5c12ef10bb62dfb55902476cdd198
Chandan Kumar <CKUMAR2@hallmark.com>
2018-03-05 13:07:57 -0600

    socketIO creation

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 /*
  * @Author: Chandan Kumar 
  * @Date: 2018-03-02 18:43:17 
- * @Last Modified by: Chandan Kumar
- * @Last Modified time: 2018-03-02 20:41:01
+ * @Last Modified by: ckumar2@hallmark.com
+ * @Last Modified time: 2018-03-05 13:07:06
  */
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname + '../public');
+const http = require('http');
+const socketIO = require('socket.io');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.port || 3000;
 
 // console.log(__dirname + '../public');
-console.log(publicPath);
+// console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 //Chandan
-app.listen(3000, () => {
+server.listen(port, () => {
 
     console.log('Server is up at 3000');
 
